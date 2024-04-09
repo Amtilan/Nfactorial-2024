98adb3dfd9d849e76aa0ce7eb1fee30941bdde86
Amtilan <149930972+Amtilan@users.noreply.github.com>
2024-04-09 17:22:30 +0500

    fix keys in def_ai_text

--- my_tengri_api/def_ai_text.py
+++ my_tengri_api/def_ai_text.py
@@ -1,42 +1,42 @@
 
-# import google.generativeai as genai
+import google.generativeai as genai
 
 
-# GOOGLE_API_KEY = "AIzaSyCPzEJyZAJ2QsIZ-FPBLPKJiVJg2bX77dg"
-# genai.configure(api_key=GOOGLE_API_KEY)
+# GOOGLE_API_KEY = <---- YOUR API KEY
+genai.configure(api_key=GOOGLE_API_KEY)
 
-# generation_config = {
-#   "temperature": 0.5,
-#   "top_p": 1,
-#   "top_k": 1,
-#   "max_output_tokens": 2048,
-# }
+generation_config = {
+  "temperature": 0.5,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
 
-# safety_settings = [
-#   {
-#     "category": "HARM_CATEGORY_HARASSMENT",
-#     "threshold": "HIGH"
-#   },
-#   {
-#     "category": "HARM_CATEGORY_HATE_SPEECH",
-#     "threshold": "HIGH"
-#   },
-#   {
-#     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-#     "threshold": "HIGH"
-#   },
-#   {
-#     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-#     "threshold": "HIGH"
-#   },
-# ]
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "HIGH"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "HIGH"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "HIGH"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "HIGH"
+  },
+]
 
-# model = genai.GenerativeModel(model_name="gemini-1.0-pro",
-#                               generation_config=generation_config,
-#                               safety_settings=safety_settings)
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
 
-# def get_ai(full_text):
-#     response = f"Твоя задача заключается в том чтобы полностью обьяснит всю важную информацию в новостной статье очень кратко и ясно.  {full_text}"
-#     text1 = model.generate_content(response)
-#     # print(text1.text)
-#     return text1.text
+def get_ai(full_text):
+    response = f"Твоя задача заключается в том чтобы полностью обьяснит всю важную информацию в новостной статье очень кратко и ясно.  {full_text}"
+    text1 = model.generate_content(response)
+    # print(text1.text)
+    return text1.text
